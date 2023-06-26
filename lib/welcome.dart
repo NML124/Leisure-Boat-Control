b1f600b5a319f395f96ad24f64330206f126bf0b
NML124 <nehemielwatumba@gmail.com>
2023-06-26 22:39:35 +0100

    creation of welcome page

---
+++ lib/welcome.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  PageController _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        PageView(
+          controller: _pageController,
+          children: [
+            Container(
+              color: Colors.blue,
+            ),
+            Container(
+              color: Colors.yellow,
+            ),
+            Container(
+              color: Colors.green,
+            ),
+          ],
+        ),
+      ],
+    ));
+  }
+}
