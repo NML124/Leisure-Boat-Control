6fb6e6e7b4ef71952a43ca7f23bae900aaf68990
NML124 <nehemielwatumba@gmail.com>
2023-06-29 02:30:40 +0100

    Add exception for login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:leisure_boat/providers/auth.dart';
 import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
-
+  final routeName = "/login";
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
@@ -12,44 +14,97 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  var errorMessage = '';
+  var _isLoading = false;
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('An error occured'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+            child: Text('Okay'),
+          ),
+        ],
+      ),
+    );
+  }
 
   Future<void> _submit() async {
-    await Provider.of<Auth>(context, listen: false).signIn(
-      emailController.text,
-      passwordController.text,
-    );
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      await Provider.of<Auth>(context, listen: false).signIn(
+        emailController.text,
+        passwordController.text,
+      );
+      Navigator.pushReplacementNamed(context, "");
+    } on HttpException catch (error) {
+      errorMessage = "Authentification failed";
+      if (error.toString().contains("EMAIL_NOT_FOUND")) {
+        errorMessage = "This email does not exist";
+      } else if (error.toString().contains("INVALID_EMAIL")) {
+        errorMessage = "This email is not valid";
+      } else if (error.toString().contains("INVALID_PASSWORD")) {
+        errorMessage = "This password is not correct";
+      } else if (error.toString().contains("USER_DISABLED")) {
+        errorMessage = "This user has been disabled";
+      }
+      _showErrorDialog(errorMessage);
+    } catch (error) {
+      const errorMessage = "Could not authenticate you. Please try again later";
+      _showErrorDialog(errorMessage);
+    }
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+      body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(25.0),
           child: Column(children: [
             Container(
                 height: 300,
                 child: Image.asset(
                   "assets/images/logIn.jpg",
                 )),
+            SizedBox(height: 20),
             TextField(
               decoration: InputDecoration(
                 labelText: "Email",
+                labelStyle: TextStyle(fontSize: 18),
               ),
               controller: emailController,
             ),
+            SizedBox(height: 20),
             TextField(
               obscureText: true,
               decoration: InputDecoration(
                 labelText: "Password",
+                labelStyle: TextStyle(fontSize: 18),
               ),
               controller: passwordController,
             ),
-            OutlinedButton(
-              onPressed: () => _submit(),
-              child: Text("Sign In"),
-            )
+            SizedBox(height: 20),
+            _isLoading
+                ? CircularProgressIndicator()
+                : OutlinedButton(
+                    onPressed: () => _submit(),
+                    child: Text(
+                      "Sign In",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
           ]),
         ),
       ),
--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
@@ -6,15 +7,22 @@ class Auth with ChangeNotifier {
   Future<void> signIn(String email, String password) async {
     const url =
         'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyClUXtWb8H5KMhdFXWHgg6y4v4jywP669Y';
-    final response = await http.post(
-      Uri.parse(url),
-      body: json.encode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      }),
-    );
 
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: json.encode({
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        }),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw (error);
+    }
   }
 }
